bf8ae8a640c277a11832ff5da688d4d3bce938b5
edpadua <ed.padua@gmail.com>
2023-07-31 11:21:23 -0300

    Border countries
    Border countries

--- countrieslist/src/app/pais/[name]/page.tsx
+++ countrieslist/src/app/pais/[name]/page.tsx
@@ -6,7 +6,9 @@ import Image from "next/image";
 
 import Link from "next/link";
 
-import { Country, CountryList } from '../../@types/types';
+import { Country } from '../../@types/types';
+
+import CountryCard from "../../Components/CountryCard/index"
 
 import axios from 'axios';
 
@@ -25,8 +27,13 @@ const Info = tw.h2`
     text-xl text-gray-800 mt-3
 `;
 
+
+const BorderTitle = tw.h3`
+      mt-12 text-2xl font-bold text-gray-800"
+`;
+
 const InfoContainerImg = tw.div`
-relative my-2 w-1/2 h-80 min-h-full  md:order-last order-first
+relative my-2 lg:w-1/2 h-80 min-h-full  md:order-last order-first
 `;
 
 
@@ -35,6 +42,14 @@ inline-block px-2 bg-blue-800 mr-2 text-white text-sm
 `;
 
 
+const PaisContainer = tw.div`
+flex flex-col container
+`;
+
+
+const BorderList = tw.div`
+grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  w-full container gap-2 mt-16 
+`;
 
 
 async function getCountryByName(name: string): Promise<Country> {
@@ -47,17 +62,36 @@ async function getCountryByName(name: string): Promise<Country> {
 
 }
 
+
+async function getBorderCountries(name: string) {
+
+
+  const url = "https://restcountries.com/v3.1/all";
+  const response = await axios.get<Country[]>(url);
+  console.log("Countries", response.data);
+  const country = response.data.find((country: Country) => country.name.common === name)!;
+
+  return country.borders?.map((border) => {
+    const borderCountry = response.data.find((country) => country.cca3 === border)!;
+    return {
+      borderCountry
+    };
+  });
+
+}
+
 async function CountryPage({
   params: { name },
 }: {
   params: { name: string };
 }) {
   const country = await getCountryByName(decodeURI(name));
+  const borderCountries = await getBorderCountries(decodeURI(name));
 
   const formatter = Intl.NumberFormat("en", { notation: "compact" });
 
   return (
-    <div className="flex flex-col container">
+    <PaisContainer>
       <NameCountry>{country.name.common}</NameCountry>
       <Link className="flex items-center py-4" href="/">
 
@@ -96,7 +130,19 @@ async function CountryPage({
           />
         </InfoContainerImg>
       </InfoCountry>
-    </div>
+      <div>
+        <BorderTitle>
+          Faz fonteira com:
+        </BorderTitle>
+        <BorderList>
+
+          {borderCountries?.map((border, index) => (
+
+            <CountryCard key={index} {...border.borderCountry} />
+          ))}
+        </BorderList>
+      </div>
+    </PaisContainer>
   )
 }
 
